99e3ab9195ed8c08015beeb3854d9686cba52534
session_ac <rabit367@163.com>
2019-03-21 15:41:22 +0800

    product model add

---
+++ ssdp/ssdp_product/src/main/java/org/ssdp_product/RunApplication.java
@@ -0,0 +1,13 @@
+package org.ssdp_product;
+
+/**
+ * Hello world!
+ *
+ */
+public class RunApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
